59230c673408f8a84f041ed473185c21a0c5adff
kimamovic21 <imamovic.kerim@gmail.com>
2022-08-01 14:50:46 +0200

    izmjene
    data.js fajl

---
+++ documentation.md
@@ -0,0 +1,16 @@
+1. instaliramo react aplikaciju
+2. izbrisemo nepotrebne fajlove
+3. u src folderu kreiramo data.js fajl
+4. u App.js importujemo data.js
+5. uredujemo App.js fajl
+
+
+
+Github pages
+1. dodajemo homepage u package.json fajlu
+    "https://kimamovic21.github.io/(ime projekta)"
+2. u terminal kucamo npm i gh-pages --save-dev
+3. u package.json fajlu dodajemo predeploy i deploy
+    "predeploy": "npm run build",
+    "deploy": "gh-pages -d build",
+4. u terminal kucamo npm run deploy
--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {contacts} from './data.js';
 
 function App() {
+
+  console.log(contacts);
+
+  const [search, setSearch] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+
+      <h1>Contact keeper</h1>
+
+      <input onChange={(e) => setSearch(e.target.value)}
+             className="searchBtn" 
+             type="search" 
+             placeholder="Search contact"
+      />
+
+      <table id="contacts">
+      <thead>
+      <tr>
+         <th>First name</th>
+         <th>Last name</th>
+         <th>Email</th>
+         <th>Phone</th>
+      </tr>
+      </thead>
+
+      <tbody>
+        {contacts.filter((contact) => {
+          return search.toLowerCase() === '' 
+          ? contact 
+          : contact.first_name.toLowerCase().includes(search)
+        })
+        .map((contact) => (
+          <tr key={contact.id}>
+            <td>{contact.first_name}</td>
+            <td>{contact.last_name}</td>
+            <td>{contact.email}</td>
+            <td>{contact.phone}</td>
+          </tr>
+        ))}
+      </tbody>
+      
+      </table>
+
+    </main>
   );
 }
 
 export default App;
+
+
+
+
+// 1. dodajemo table html element
+// 2. dodajemo thead i tbody elemente
+// 3. unutar thead i tbody elemenata dodajemo tr elemente i unutar njih td elemente
+// 4. unutar tbody elementa dodajemo javascript map metodu
+// 5. uredujemo td elemente
+// 6. dodajemo react hook useState
+// 7. input elementu dodajemo onChange dogadaj
+// 8. dodajemo javascript filter metodu (prije map metode)
+// 9. dodajemo ternarni operator 
